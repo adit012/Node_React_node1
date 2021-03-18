7367052c99afa536f3a8dbea70656fdde93f8950
adit012 <80872781+adit012@users.noreply.github.com>
2021-03-18 19:03:33 +0700

    Node_React_node1

---
+++ index.js
@@ -0,0 +1,12 @@
+//inisialisasi library
+const express = require("express")
+const app = express()
+
+//import route pegawai
+const pegawai = require("./route/pegawai")
+app.use("/pegawai",pegawai)
+
+//membuat web server dengan port 2000
+app.listen(2020, () => {
+    console.log("server run on port 2020")
+})
